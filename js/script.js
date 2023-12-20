c862de300917274f99306e45cd8bd6d61e29fe11
AimanFadil <aimanbello2001@gmail.com>
2023-12-20 17:44:40 +0100

    fine es

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue;
+   
+createApp({
+    data(){
+        return{
+            mail: []
+        }
+    },
+    methods:{
+        getRandomMail(){
+            for(let i = 0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( ( response ) =>{
+                    this.mail.push(response.data.response)
+                })
+            }
+        }
+    }
+}).mount('#app')
